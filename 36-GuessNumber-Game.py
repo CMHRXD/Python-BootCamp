9de135853c005ad49cdad0061b3aa0be130c03b8
CMHRXD <cmhrlxl@gmail.com>
2023-08-26 14:39:05 +0100

    Upgrading game

--- 36-GuessNumber-Game.py
+++ 36-GuessNumber-Game.py
@@ -1,5 +1,13 @@
 # GuessNumber Game
 import random as r
+from replit import clear
+
+logo = """
+  / _ \_   _  ___  ___ ___  /__   \ |__   ___    /\ \ \_   _ _ __ ___ | |__   ___ _ __ 
+ / /_\/ | | |/ _ \/ __/ __|   / /\/ '_ \ / _ \  /  \/ / | | | '_ ` _ \| '_ \ / _ \ '__|
+/ /_\\| |_| |  __/\__ \__ \  / /  | | | |  __/ / /\  /| |_| | | | | | | |_) |  __/ |   
+\____/ \__,_|\___||___/___/  \/   |_| |_|\___| \_\ \/  \__,_|_| |_| |_|_.__/ \___|_|  
+"""
 
 attempts_object = {
     "1": 10, # easy
@@ -8,6 +16,8 @@ attempts_object = {
 }
 
 def init():
+    clear()
+    print(logo)
     print("Welcome to GuessNumber Game!")
     print("You have to guess a number between 1 and 100")
     dificulty = input("Choose a dificulty: \n1 - Easy\n2 - Medium\n3 - Hard\n")
@@ -26,6 +36,12 @@ def init():
             
         attempts -= 1
         print(f"You have {attempts} attempts left")
+    
+    option = input("Do you want to play again? (y/n): ")
+    if option == "y":
+        init()
+    else:
+        print("Good bye!")
 
 init()
 
